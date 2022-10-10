913819c80a2923d24735dfedd46f5dadb25e444d
Azeem <livelyazeem@gmail.com>
2022-10-10 17:25:51 +0500

    learning chap 2

--- 2/index.js
+++ 2/index.js
@@ -10,6 +10,25 @@ const fileOps = async () => {
       "utf-8"
     );
     console.log(data);
+    // fs.unlink == delete
+    await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"));
+    await fsPromises.writeFile(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      data
+    );
+    await fsPromises.appendFile(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      "\n\nNice to meet you."
+    );
+    await fsPromises.rename(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      path.join(__dirname, "files", "promiseComplete.txt")
+    );
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "files", "promiseComplete.txt"),
+      "utf-8"
+    );
+    console.log(newData);
   } catch (err) {
     console.error(err);
   }
