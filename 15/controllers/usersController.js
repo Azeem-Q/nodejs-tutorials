f41e18d2adb3bd8f8978c29f9a12b4bb4af18836
Azeem <livelyazeem@gmail.com>
2023-01-13 08:46:26 +0500

    doing final assignment

---
+++ 15/controllers/usersController.js
@@ -0,0 +1,25 @@
+const User = require("../model/User");
+
+const getAllusers = async (req, res) => {
+  const users = await User.find();
+  if (!users) return res.status(204).json({ message: "No users found" });
+  res.json(users);
+};
+
+const deleteUser = async (req, res) => {
+  if (!req?.body?.id)
+    return res.status(400).json({ message: "User ID required." });
+  const User = await User.findOne({ _id: req.body.id }).exec();
+  if (!User) {
+    return res
+      .status(204)
+      .json({ message: `No User matches ID ${req.body.id}` });
+  }
+  const result = await User.deleteOne({ _id: req.body.id });
+  res.json(result);
+};
+
+module.exports = {
+  getAllusers,
+  deleteUser,
+};
